a1a106ecc50db94703a5bd6689deb73039c2b8c9
Fazlul Alam <fazlulalam619@gmail.com>
2024-04-12 20:12:51 +0600

    Register Page

--- src/pages/Shared/Register/Register.jsx
+++ src/pages/Shared/Register/Register.jsx
@@ -1,7 +1,82 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    console.log(form.get("password"));
+  };
   return (
     <div>
-      <h1>Register</h1>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <form onSubmit={handleRegister} className="card-body">
+            <p className=" text-center font-bold">Please Register</p>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Photo Url</span>
+              </label>
+              <input
+                type="text"
+                name="photoUrl"
+                placeholder="Photo Url"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Login</button>
+            </div>
+          </form>
+          <p className="card-body text-center">
+            Already Have An Account?
+            <Link className="font-bold text-blue-900" to="/login">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
